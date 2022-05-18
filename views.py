81c1f862c5581e64fa68396388a870fd84c501f7
Otieno100 <brian.masafu@student.moringaschool.com>
2022-05-18 20:03:54 +0300

    added new untracked files from database and git ignore

--- views.py
+++ views.py
@@ -1,8 +1,7 @@
-from crypt import methods
-# from quote import app 
+
 import email
 import bcrypt
-# from flask_login import LoginManager, UserMixin,current_user,login_manage
+from flask_login import LoginManager, UserMixin,current_user,login_manager
 from flask import Flask, redirect, render_template, request,flash, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -21,7 +20,28 @@ from flask import render_template
 from  quote import  quote
 from quote import request
 from werkzeug.utils import secure_filename
+
+
+from crypt import methods
+# from quote import app 
+
 from email.mime import image
+
+from flask_login import LoginManager, UserMixin,current_user,login_manager
+
+from flask import Flask, redirect, render_template, request,flash, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField, PasswordField
+from wtforms.validators import DataRequired
+# from flask_migrate import Migrate
+
+from wtforms import StringField,SubmitField,PasswordField,FileField,TextAreaField,EmailField
+from wtforms.validators import DataRequired
+
+import os
+
 
 
 
@@ -56,6 +76,7 @@ mail=Mail(app)
 bcrypt=Bcrypt(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+login_manager = LoginManager(app)
 
 
 
@@ -67,12 +88,17 @@ class posts(db.Model):
 
 
 
-class post(db.Model):
+class postss(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(25),nullable =False) 
     post = db.Column(db.String(50),nullable =False)
  
 
+class Post(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(50),nullable=False)
+    post=db.Column(db.String(700),nullable=False)
+    poster=db.Column(db.Integer,db.ForeignKey('user.id'))
 
 
 
@@ -114,27 +140,28 @@ class RegisterFrm(FlaskForm):
 
   
 
-class User(db.Model) :
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(30),nullable = False) 
-    email = db.Column(db.String(20),nullable = False)
-    password = db.Column(db.String(200), nullable = False)
 
 
 
-
-    
-
-@app.route('/')
-def home():
-    quotes=[]
-    req=request.Request()
-    data=req.request("http://quotes.stormconsultancy.co.uk/random.json",500)
-    quotes.append(quote.Quote(data["id"],data["quote"],data["author"]))
-        
+class User(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(40),nullable=False)
+    email=db.Column(db.String(40),nullable=False)
+    password=db.Column(db.String(40),nullable=False)
+    postman=db.relationship('Post',backref="postman")
 
 
-    return render_template('index.html',datum=quotes ,)
+
+class Images(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50),nullable=False)
+    uploader_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+
+
+class UploadForm(FlaskForm):
+    file=FileField('Appartment',validators=[DataRequired()])
+    submt=SubmitField('Upload Now')    
+
 
 
 
@@ -183,7 +210,7 @@ def login():
 
 @app.route('/display')
 def display():
-    qr_all = post.query.all()
+    qr_all = postss.query.all()
 
     return render_template('display.html', data = qr_all)
 
@@ -198,20 +225,72 @@ def index():
 
 
     if dataForm.validate_on_submit():
-        addpost = post(post = dataForm.post.data, title = dataForm.title.data)
+        addpost = postss(post = dataForm.post.data, title = dataForm.title.data)
         db.session.add(addpost)
         db.session.commit()
 
         return redirect(url_for('display'))
     return render_template('update.html',form = dataForm)    
 
+# trace
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 
 
+@app.route("/image",methods=["POST","GET"])
+def uploadimage():
+    # user=current_user
+    frm=UploadForm()
+    if frm.validate_on_submit():
+        file=request.files["file"]
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"],secure_filename(file.filename)))
+        upload=Images(name=secure_filename(file.filename))
+        db.session.add(upload)
+        db.session.commit()
+        return redirect(url_for("viewimage"))
+    return render_template("image.html",form=frm)
 
-if __name__=='__main__':
-    app.run(debug=1)
+
+
+
+    
+
+@app.route("/allimages",methods=["POST","GET"])
+def viewallimages():
+    allimages=Images.query.all()
+    return render_template("allimages.html",images=allimages)
+
+@app.route("/viewimage",methods=["POST","GET"])
+def viewimage():
+    allimages=Images.query.all()
+    return render_template("imageview.html",images = allimages)
+
+@app.route('/')
+def home():
+    quotes=[]
+    req=request.Request()
+    data=req.request("http://quotes.stormconsultancy.co.uk/random.json",500)
+    quotes.append(quote.Quote(data["id"],data["quote"],data["author"]))
+        
+
+
+    return render_template('index.html',datum=quotes ,)
+
+
+
+
+#trace
+
+
+
+# if __name__=='__main__':
+#     app.run(debug=1)
+
 
 
 
